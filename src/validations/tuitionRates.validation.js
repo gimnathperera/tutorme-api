650ead224d45f8ac0ad1a91a855abda5bab58c8b
piyumikajayasiri <malini.softvilmedia@gmail.com>
2026-04-27 10:38:45 +0530

    fix: changing the tuition rates payload

--- src/validations/tuitionRates.validation.js
+++ src/validations/tuitionRates.validation.js
@@ -1,39 +1,31 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
+const rateRange = Joi.object().keys({
+  minimumRate: Joi.number().positive().required(),
+  maximumRate: Joi.number().positive().required(),
+});
+
+const idValidator = Joi.string().custom((value, helpers) => {
+  if (!mongoose.Types.ObjectId.isValid(value)) {
+    return helpers.message('Invalid ID');
+  }
+  return value;
+});
+
 // Create tuition rate validation
 const createTuitionRate = {
   body: Joi.object().keys({
     subject: Joi.string().required(),
     grade: Joi.string().required(),
-    onlineIndividualTuitionRate: Joi.array().items(
-      Joi.object().keys({
-        minimumRate: Joi.string().required(),
-        maximumRate: Joi.string().required(),
-      })
-    ),
-    onlineGroupTuitionRate: Joi.array().items(
-      Joi.object().keys({
-        minimumRate: Joi.string().required(),
-        maximumRate: Joi.string().required(),
-      })
-    ),
-    physicalIndividualTuitionRate: Joi.array().items(
-      Joi.object().keys({
-        minimumRate: Joi.string().required(),
-        maximumRate: Joi.string().required(),
-      })
-    ),
-    physicalGroupTuitionRate: Joi.array().items(
-      Joi.object().keys({
-        minimumRate: Joi.string().required(),
-        maximumRate: Joi.string().required(),
-      })
-    ),
+    universityStudentsRate: rateRange.required(),
+    partTimeTutorRate: rateRange.required(),
+    fullTimeTutorRate: rateRange.required(),
+    moeTeacherRate: rateRange.required(),
   }),
 };
 
-// Get tuition rates validation (supports filtering)
+// Get tuition rates validation
 const getTuitionRates = {
   query: Joi.object().keys({
     subject: Joi.string(),
@@ -47,53 +39,23 @@ const getTuitionRates = {
 // Get a single tuition rate validation
 const getTuitionRate = {
   params: Joi.object().keys({
-    tuitionRatesId: Joi.string().custom((value, helpers) => {
-      if (!mongoose.Types.ObjectId.isValid(value)) {
-        return helpers.message('Invalid ID');
-      }
-      return value;
-    }),
+    tuitionRatesId: idValidator,
   }),
 };
 
 // Update tuition rates validation
 const updateTuitionRates = {
   params: Joi.object().keys({
-    tuitionRatesId: Joi.string().custom((value, helpers) => {
-      if (!mongoose.Types.ObjectId.isValid(value)) {
-        return helpers.message('Invalid ID');
-      }
-      return value;
-    }),
+    tuitionRatesId: idValidator,
   }),
   body: Joi.object()
     .keys({
       subject: Joi.string(),
       grade: Joi.string(),
-      onlineIndividualTuitionRate: Joi.array().items(
-        Joi.object().keys({
-          minimumRate: Joi.string().required(),
-          maximumRate: Joi.string().required(),
-        })
-      ),
-      onlineGroupTuitionRate: Joi.array().items(
-        Joi.object().keys({
-          minimumRate: Joi.string().required(),
-          maximumRate: Joi.string().required(),
-        })
-      ),
-      physicalIndividualTuitionRate: Joi.array().items(
-        Joi.object().keys({
-          minimumRate: Joi.string().required(),
-          maximumRate: Joi.string().required(),
-        })
-      ),
-      physicalGroupTuitionRate: Joi.array().items(
-        Joi.object().keys({
-          minimumRate: Joi.string().required(),
-          maximumRate: Joi.string().required(),
-        })
-      ),
+      universityStudentsRate: rateRange,
+      partTimeTutorRate: rateRange,
+      fullTimeTutorRate: rateRange,
+      moeTeacherRate: rateRange,
     })
     .min(1),
 };
@@ -101,12 +63,7 @@ const updateTuitionRates = {
 // Delete tuition rates validation
 const deleteTuitionRates = {
   params: Joi.object().keys({
-    tuitionRatesId: Joi.string().custom((value, helpers) => {
-      if (!mongoose.Types.ObjectId.isValid(value)) {
-        return helpers.message('Invalid ID');
-      }
-      return value;
-    }),
+    tuitionRatesId: idValidator,
   }),
 };
 
